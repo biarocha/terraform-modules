67aa51d372d41d040800c5d1fcd7daa8e271f16d
Bianca Rocha <bia.rocha3360@outlook.com>
2023-04-08 20:47:29 -0300

    ajuste var

--- networking/networking.tf
+++ networking/networking.tf
@@ -1,13 +1,13 @@
 resource "azurerm_network_security_group" "nsg" {
   name = var.nsg_name
   location = var.location
-  resource_group_name = azurerm_resource_group.rg.name
+  resource_group_name = var.resource_group_name
 }
 
 resource "azurerm_virtual_network" "vnet" {
   name = var.vnet_name
   location = var.location
-  resource_group_name = azurerm_resource_group.rg.name
+  resource_group_name = var.resource_group_name
   address_space = var.vnet_address_space
   subnet {
     name = var.subnet_name
